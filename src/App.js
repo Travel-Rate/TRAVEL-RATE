ae8f987fc4ad4f8a12f65a8e88ba2983572afae5
JOSANGMIN777 <nice6122@naver.com>
2024-08-29 21:27:24 +0900

    feat:dev-pull-test

--- src/App.js
+++ src/App.js
@@ -2,21 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const user = false
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='nav-bar'>
+          <img src="logo" alt="" />
+          <div>여행지 추천</div>
+          <div>목표환율 설정</div>
+          <div>{user? '로그인' : '로그아웃'}</div>
+        </div>
       </header>
     </div>
   );
