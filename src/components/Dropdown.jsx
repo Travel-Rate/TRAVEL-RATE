260beef570516158063d09104c6734d7e230d386
JOSANGMIN777 <nice6122@naver.com>
2024-08-30 19:44:21 +0900

    feat:components-changeto-pages

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -34,6 +34,7 @@ function Dropdown () {
         'SEK 스웨덴'
     ]
     return (
+<<<<<<< HEAD
                 <>
                 {countries.map((country, index) => (
                     <option key={index} className={styles.dropdownItem}
@@ -43,6 +44,35 @@ function Dropdown () {
                 ))}
                 </>
    
+=======
+        <ul>
+            <li>CAD 캐나다</li>
+            <li>USD 미국</li>
+            <li>AED 아랍에미리트</li>
+            <li>BHD 바레인</li>
+            <li>BND 브루나이</li>
+            <li>CNH 중국</li>
+            <li>HKD 홍콩</li>
+            <li>IDR 인도네시아</li>
+            <li>JPY 일본</li>
+            <li>KRW 한국</li>
+            <li>KWD 쿠웨이트</li>
+            <li>MYR 말레이시아</li>
+            <li>SAR 사우디아라비아</li>
+            <li>SGD 싱가포르</li>
+            <li>THB 태국</li>
+            <li>AUD 호주</li>
+            <li>NZD 뉴질랜드</li>
+            <li>CHF 스위스</li>
+            <li>DKK 덴마크</li>
+            <li>EUR 유로</li>
+            <li>GBP 영국</li>
+            <li>NOK 노르웨이</li>
+            <li>SEK 스웨덴</li>
+
+        </ul>
+
+>>>>>>> 06c17cb (feat:components-changeto-pages)
     )
 }
 
