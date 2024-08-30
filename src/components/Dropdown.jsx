06c17cb57a21928a293823bc48257a8b14bcdcb0
JOSANGMIN777 <nice6122@naver.com>
2024-08-30 19:44:21 +0900

    feat:components-changeto-pages

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -2,20 +2,32 @@ import React from "react";
 
 export function Dropdown() {
     return (
-        <>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </>
+        <ul>
+            <li>CAD 캐나다</li>
+            <li>USD 미국</li>
+            <li>AED 아랍에미리트</li>
+            <li>BHD 바레인</li>
+            <li>BND 브루나이</li>
+            <li>CNH 중국</li>
+            <li>HKD 홍콩</li>
+            <li>IDR 인도네시아</li>
+            <li>JPY 일본</li>
+            <li>KRW 한국</li>
+            <li>KWD 쿠웨이트</li>
+            <li>MYR 말레이시아</li>
+            <li>SAR 사우디아라비아</li>
+            <li>SGD 싱가포르</li>
+            <li>THB 태국</li>
+            <li>AUD 호주</li>
+            <li>NZD 뉴질랜드</li>
+            <li>CHF 스위스</li>
+            <li>DKK 덴마크</li>
+            <li>EUR 유로</li>
+            <li>GBP 영국</li>
+            <li>NOK 노르웨이</li>
+            <li>SEK 스웨덴</li>
+
+        </ul>
 
     )
 }
--- src/pages/ExchangeRatio.jsx
+++ src/pages/ExchangeRatio.jsx
@@ -1,21 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { Dropdown } from "../components/Dropdown";
 
 const ExchangeRatio = () => {
+    const [view, setView] = useState(false);
+
     return (
         <div className="ExchangeRatio">
             <header></header>
             <body>
                 <div className="upper-side-contents">
                 <span>도움말 설정</span><div className="help-check-box"></div>
-                <span>목표환율 설정</span><option value="country" className="upper-side-inner-select-country">
-                    
+                <span>목표환율 설정</span><option onClick={()=>{setView(!view)}} value="country" className="upper-side-inner-select-country">
+                    {view? '⌃':'⌄'}
+                    {view && <Dropdown/>}
                 </option>
                 <div>1250$</div>
                 <img src="" alt="plus-img" />
                  <p>환율알림 추가</p>
                 </div>
                 <div className="lower-side-contents">
-
+                    <div>
+                        <span>최대 3개국/오전 11시 마다 업데이트</span>
+                        <span>환율 수정 / 삭제</span>
+                    </div>
+                    <div className="lower-side-inner-country1">
+                        <div>
+                        <img src="" alt="" />
+                        <span></span>
+                        </div>
+                        <div>
+                            <img src="" alt="modify" />
+                            <img src="" alt="delete" />
+                        </div>
+                    </div>
                 </div>
             </body>
         </div>
