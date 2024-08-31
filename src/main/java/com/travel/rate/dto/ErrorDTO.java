7dc8c105642a31f0b7f78b394eeae7604e5f2028
jisoo <ske050588@gmail.com>
2024-08-31 22:23:58 +0900

    feat: 에러dto, 로그인dto

---
+++ src/main/java/com/travel/rate/dto/ErrorDTO.java
@@ -0,0 +1,12 @@
+package com.travel.rate.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Data;
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@Data
+public class ErrorDTO {
+    String message;
+    int code;
+}
