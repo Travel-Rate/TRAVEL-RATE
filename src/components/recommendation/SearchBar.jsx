5340280d34767c4eced92fa527c10e872a24916a
hana <gksk3185@gmail.com>
2024-09-01 00:45:33 +0900

    feat: Recommendation page 1

--- src/components/recommendation/SearchBar.jsx
+++ src/components/recommendation/SearchBar.jsx
@@ -1,16 +1,24 @@
+<<<<<<< HEAD
 import React, { useState, useEffect, useRef } from "react"
 import styles from './SearchBar.module.scss'
+=======
+import React, {useState, useEffect, useRef} from "react";
+import styles from './SearchBar.module.scss';
+>>>>>>> 60918ff (feat: Recommendation page 1)
 
 export const SearchBar = ({ onSearchClick }) => {
     const [activeIndex, setActiveIndex] = useState(0)
     const [isTabVisible, setIsTabVisible] = useState(false)
     const [isTabVisible2, setIsTabVisible2] = useState(false)
+<<<<<<< HEAD
 
     const [isTravelClicked, setIsTravelClicked] = useState(false)
     const [isBudgetClicked, setIsBudgetClicked] = useState(false)
 
     const [isHovered, setIsHovered] = useState(false)
 
+=======
+>>>>>>> 60918ff (feat: Recommendation page 1)
     const tabRef = useRef(null)
     const tabRef2 = useRef(null)
 
@@ -19,6 +27,7 @@ export const SearchBar = ({ onSearchClick }) => {
     }
 
     const tabContArr = [
+<<<<<<< HEAD
         { index: 0, tabTitle: "전체", tabCont: ["중국", "홍콩", "일본", "브루나이", "인도네시아", "말레이시아", "싱가포르", "태국", "아랍에미리트", "바레인", "쿠웨이트", "사우디", "미국", "캐나다", "유로존", "스위스", "덴마크", "영국", "노르웨이", "스웨덴", "호주", "뉴질랜드"] },
         { index: 1, tabTitle: "동북아", tabCont: ["중국", "홍콩", "일본"] },
         { index: 2, tabTitle: "동남아", tabCont: ["브루나이", "인도네시아", "말레이시아", "싱가포르", "태국"] },
@@ -27,33 +36,94 @@ export const SearchBar = ({ onSearchClick }) => {
         { index: 5, tabTitle: "유럽", tabCont: ["유로존", "스위스", "덴마크", "영국", "노르웨이", "스웨덴"] },
         { index: 6, tabTitle: "오세안주", tabCont: ["호주", "뉴질랜드"] }
     ];
+=======
+        {
+            index: 0,
+            tabTitle: "전체",
+            tabCont: ["중국", "홍콩", "일본", 
+                "브루나이", "인도네시아", "말레이시아", "싱가포르", "태국", 
+                "아랍에미리트", "바레인", "쿠웨이트", "사우디",
+                "미국", "캐나다",
+                "유로존", "스위스", "덴마크", "영국", "노르웨이", "스웨덴",
+                "호주", "뉴질랜드"
+                ]
+        },
+        {
+            index: 1,
+            tabTitle: "동북아",
+            tabCont: ["중국", "홍콩", "일본"]
+        },
+        {
+            index: 2,
+            tabTitle: "동남아",
+            tabCont: ["브루나이", "인도네시아", "말레이시아", "싱가포르", "태국"]
+        },
+        {
+            index: 3,
+            tabTitle: "중동",
+            tabCont: ["아랍에미리트", "바레인", "쿠웨이트", "사우디"]
+        },
+        {
+            index: 4,
+            tabTitle: "북미",
+            tabCont: ["미국", "캐나다"]
+        },
+        {
+            index: 5,
+            tabTitle: "유럽",
+            tabCont: ["유로존", "스위스", "덴마크", "영국", "노르웨이", "스웨덴"]
+        },
+        {
+            index: 6,
+            tabTitle: "오세안주",
+            tabCont: ["호주", "뉴질랜드"]
+        },
+    ]
+>>>>>>> 60918ff (feat: Recommendation page 1)
 
     const handleClickOutside = (event) => {
         if (tabRef.current && !tabRef.current.contains(event.target)) {
             setIsTabVisible(false)
+<<<<<<< HEAD
             setIsTravelClicked(false)
+=======
+>>>>>>> 60918ff (feat: Recommendation page 1)
         }
     }
 
     const handleClickOutside2 = (event) => {
         if (tabRef2.current && !tabRef2.current.contains(event.target)) {
             setIsTabVisible2(false)
+<<<<<<< HEAD
             setIsBudgetClicked(false)
         }
     };
 
     useEffect(() => {
+=======
+        }
+    }
+
+    useEffect(()=> {
+>>>>>>> 60918ff (feat: Recommendation page 1)
         document.addEventListener("click", handleClickOutside)
         return () => {
             document.removeEventListener("click", handleClickOutside)
         }
+<<<<<<< HEAD
     }, [])
 
     useEffect(() => {
+=======
+    },[])
+
+    useEffect(()=> {
+>>>>>>> 60918ff (feat: Recommendation page 1)
         document.addEventListener("click", handleClickOutside2)
         return () => {
             document.removeEventListener("click", handleClickOutside2)
         }
+<<<<<<< HEAD
     }, [])
 
     const searchBarClass = `${styles.SearchBar} ${isTravelClicked || isBudgetClicked ? styles.inactive : ''}`
@@ -70,6 +140,17 @@ export const SearchBar = ({ onSearchClick }) => {
                         setIsTravelClicked(true)
                         setIsBudgetClicked(false)
                     }}
+=======
+    },[])
+
+
+    return (
+        <div className={styles.MainContainer}>
+            <form className={styles.SearchBar}>
+                <div 
+                    className={styles.TravelDestinationContainer}
+                    onClick={() => setIsTabVisible(true)}
+>>>>>>> 60918ff (feat: Recommendation page 1)
                 >
                     <p className={styles.TravelDestination}>여행지</p>
                     <p className={styles.TravelDestinationSearch}>여행지 검색</p>
@@ -80,18 +161,30 @@ export const SearchBar = ({ onSearchClick }) => {
                         <ul className={styles.TabList}>
                             {tabContArr.map((section, index) => (
                                 <p
+<<<<<<< HEAD
                                     key={index}
                                     className={`${styles.TabItem} ${index === activeIndex ? styles.isActive : ''}`}
                                     onClick={() => tabClickHandler(index)}
                                 >
                                     {section.tabTitle}
+=======
+                                key={index}
+                                className={`${styles.TabItem} ${index === activeIndex ? styles.isActive : ''}`}
+                                onClick={() => tabClickHandler(index)}
+                                >
+                                {section.tabTitle}
+>>>>>>> 60918ff (feat: Recommendation page 1)
                                 </p>
                             ))}
                             <div className={styles.ClickSlideWrapper}>
                                 <div className={styles.ClickSlide} style={{ left: `${activeIndex * 14.2857}%` }}></div>
                             </div>
                         </ul>
+<<<<<<< HEAD
 
+=======
+                        
+>>>>>>> 60918ff (feat: Recommendation page 1)
                         <div>
                             {Array.isArray(tabContArr[activeIndex].tabCont) ? (
                                 tabContArr[activeIndex].tabCont.map((item, idx) => (
@@ -107,6 +200,7 @@ export const SearchBar = ({ onSearchClick }) => {
                         </div>
                         <button className={styles.ContinentBtn}>대륙 선택</button>
                     </div>
+<<<<<<< HEAD
                 )}
 
                 <div
@@ -118,6 +212,14 @@ export const SearchBar = ({ onSearchClick }) => {
                     onMouseEnter={() => setIsHovered(true)}
                     onMouseLeave={() => setIsHovered(false)}
                     className={`${styles.BudgetContainer} ${isBudgetClicked ? styles.clicked : isTravelClicked ? styles.inactive : ''}`}
+=======
+
+                )}
+
+                <div
+                    onClick={() => setIsTabVisible2(true)}
+                    className={styles.BudgetContainer}
+>>>>>>> 60918ff (feat: Recommendation page 1)
                 >
                     <p className={styles.Budget}>예산</p>
                     <p className={styles.BudgetInput}>예산 입력</p>
@@ -134,15 +236,28 @@ export const SearchBar = ({ onSearchClick }) => {
                 )}
 
                 <button
+<<<<<<< HEAD
                     type="button"
                     className={styles.SearchBtn}
                     onClick={onSearchClick}
+=======
+                type="button"
+                className={styles.SearchBtn}
+                onClick={onSearchClick}
+>>>>>>> 60918ff (feat: Recommendation page 1)
                 >
                     <img className={styles.SearchImg} src="/search.svg" alt="searchButton" />
                 </button>
             </form>
+<<<<<<< HEAD
 
             <div className={styles.underline}></div>
         </div>
+=======
+        
+        <div className={styles.underline}></div>
+
+    </div>
+>>>>>>> 60918ff (feat: Recommendation page 1)
     )
 }
