3605f59382af5619e613fd0cfb8737054c78e2c9
jisoo <ske050588@gmail.com>
2024-09-01 03:55:02 +0900

    feat: 환율기반 나라 추천 로직 완료

---
+++ src/main/java/com/travel/rate/dto/travel/ResTravelDTO.java
@@ -0,0 +1,15 @@
+package com.travel.rate.dto.travel;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.travel.rate.domain.Country;
+import com.travel.rate.domain.Currency;
+import lombok.Data;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@Data
+public class ResTravelDTO {
+    Long budget;
+    Currency currency;// currency안에 country 존재?
+    Country country;
+}
--- src/main/java/com/travel/rate/service/TravelService.java
+++ src/main/java/com/travel/rate/service/TravelService.java
@@ -2,6 +2,7 @@ package com.travel.rate.service;
 
 import com.travel.rate.domain.Country;
 import com.travel.rate.domain.Currency;
+import com.travel.rate.dto.travel.ResTravelDTO;
 import com.travel.rate.repository.CountryRepository;
 import com.travel.rate.utils.ExchangeUtils;
 import lombok.RequiredArgsConstructor;
@@ -24,11 +25,13 @@ public class TravelService {
 
         List<Currency> top3Currencies = getTop3Currencies(countryList);
 
+        top3Currencies = sortedBySeasonAndFilteredByCtntId(top3Currencies, ctnt_id);
 
+        //TODO 정렬완성된 것에서 currency당 1등을 RES로 변환
 
     }
 
-    public List sortedBySeasonAndFilteredByCtntId(List<Currency> currencies, Long ctnt_id){
+    public String getCurrentSeason(){
         int currentMonth = LocalDate.now().getDayOfMonth();
         String currentSeason = "";
 
@@ -37,15 +40,36 @@ public class TravelService {
         else if(currentMonth <= 11) currentSeason = "가을";
         else currentSeason = "겨울";
 
-        for(Currency currency : currencies){
-            currency.getCountries().stream()
-                    .filter(country -> country.getContinent().getCtntId() == ctnt_id)
-                    .sorted()//TODO sort 로직 구현
-                    .collect(Collectors.toList());
+        return currentSeason;
+    }
+
+    public List<Currency> sortedBySeasonAndFilteredByCtntId(List<Currency> currencies, Long ctntId){
+
+        String currentSeason = getCurrentSeason();
+
+        for(Currency currency : currencies) {
+            String finalCurrentSeason = currentSeason;
+            currency.getCountries().sort((Country country1, Country country2)-> {
+                // 현재 계절일 경우 랜덤으로 점수를 받고 점수 '오름차순' 정렬
+                int country1Point = 0;
+                int country2Point = 0;
+
+                if(country1.getSeason().matches(finalCurrentSeason)){
+                    country1Point = new Random().nextInt(9)+1;
+                }
+                if(country1.getSeason().matches(finalCurrentSeason)){
+                    country1Point = new Random().nextInt(9)+1;
+                }
+
+                // 선택했던 대륙이 아닐 경우 최대 벌점을 주어 꼴등
+                if(country1.getContinent().getCtntId() != ctntId) country1Point = Integer.MAX_VALUE;
+                if(country2.getContinent().getCtntId() != ctntId) country1Point = Integer.MAX_VALUE;
+
+                return country1Point - country2Point;
+            });
         }
 
-
-        return null;
+        return currencies;
     }
 
     public List<Currency> getTop3Currencies(List<Country> countryList){
