634a61e913b611919d21f4e1e1b10a2bcb8bac13
hana <gksk3185@gmail.com>
2024-09-03 18:06:59 +0900

    fix: merge error

--- src/pages/ExchangeRatio.jsx
+++ src/pages/ExchangeRatio.jsx
@@ -1,21 +1,9 @@
-<<<<<<< HEAD
-import React, { useState} from "react";
-import Dropdown from "../components/Dropdown";
-=======
 import React, { useRef, useState} from "react";
 import Dropdown from "../components/exchangeRatio/Dropdown";
->>>>>>> features/exchage-ratio-section
 import styles from '../css/ExchangeRatio.module.scss'
 import Slider from "../components/exchangeRatio/Slider";
 
 const ExchangeRatio = () => {
-<<<<<<< HEAD
-    
-    const [helpActive, setHelpActive] = useState(false)
-    const toggleHelp = () => {
-        setHelpActive(!helpActive);
-    }
-=======
     const [countries, setCountries] = useState("")
     const [helpActive, setHelpActive] = useState(false)
     const [goalRate, setGoalRate] = useState("")
@@ -57,7 +45,6 @@ const ExchangeRatio = () => {
     }
         
     }
->>>>>>> features/exchage-ratio-section
 
     return (
         <div className={styles.ExchangeRatio}>
@@ -73,12 +60,8 @@ const ExchangeRatio = () => {
 
                 <div className={styles.goalContents}>
                 <p className={styles.goalSetting}>목표환율 설정</p>
-<<<<<<< HEAD
-                <select name="" id="" className={styles.dropdownContainer}>
-=======
                 <select onChange={handleChangeCountry} name="" id=""  className={styles.dropdownContainer}>
                     <option value="">국가를 선택해주세요</option>
->>>>>>> features/exchage-ratio-section
                     <Dropdown/>
                 </select>
                 </div>
@@ -90,11 +73,7 @@ const ExchangeRatio = () => {
 
 
                 </div>
-<<<<<<< HEAD
-                <div>
-=======
                 <div onChange={handleChangeRate}>
->>>>>>> features/exchage-ratio-section
                 <Slider/>
                 {helpActive && (
                         <div className={styles.helpText2}>
@@ -104,11 +83,7 @@ const ExchangeRatio = () => {
                     )}
                 </div>
                 <div className={styles.alarmPlus}>
-<<<<<<< HEAD
-                <img src="/add.png" alt="plus-img" />
-=======
                 <img src="/add.png" alt="plus-img" onClick={handleSubmit} />
->>>>>>> features/exchage-ratio-section
                  <p>환율알림 추가</p>
                  {helpActive && (
                         <div className={styles.helpText3}>
@@ -116,21 +91,6 @@ const ExchangeRatio = () => {
                         </div>
                     )}
                 </div>
-<<<<<<< HEAD
-                <div className={styles.lowerSideContents}>
-                    <div className={styles.alarmDialogues}>
-                        <span className={styles.alarmDialogueSmall}>최대 3개국/오전 11시 마다 업데이트</span>
-                        <span className={styles.alarmDialogueBig}>환율 수정 / 삭제</span>
-                       
-                    </div>
-                    <div className={styles.lowerSideContentsInner}>
-                        <div className={styles.lowerSideCountry1} >
-                        <img src="/flag-sample.png" alt="flag1" className={styles.flag1}/>
-                        <p className={styles.lowerSideCountry1Name}>국가명 통화코드 환율</p>
-                            <img src='/modify.png' alt="modify"className={styles.country1Modify} />
-                            <img src="/cancel.png" alt="delete" className={styles.country1Delete}/>
-                        </div>
-=======
                     <div className={styles.alarmDialogues}>
                         <span className={styles.alarmDialogueSmall}>최대 3개국/오전 11시 마다 업데이트</span>
                         <span className={styles.alarmDialogueBig}>환율 수정 / 삭제</span>
@@ -152,7 +112,6 @@ const ExchangeRatio = () => {
                             <img src='/modify.png' alt="modify"className={styles.country1Modify} />
                             <img src="/cancel.png" alt="delete" className={styles.country1Delete}/>
                         </div>}
->>>>>>> features/exchage-ratio-section
                         <hr />
                         <div className={styles.lowerSideCountry2}>
                         <img src="/flag-sample2.png" alt="flag2"  className={styles.flag2} />
@@ -166,13 +125,8 @@ const ExchangeRatio = () => {
                         <p className={styles.lowerSideCountry3Name}>국가명 통화코드 환율</p>
                             <img src="/modify.png" alt="modify" className={styles.country3Modify}/>
                             <img src="/cancel.png" alt="delete" className={styles.country3Delete}/>
-<<<<<<< HEAD
-                        </div>
-                        <hr />
-=======
                         </div> */}
                        
->>>>>>> features/exchage-ratio-section
                     </div>
                 </div>
                 {helpActive && (
